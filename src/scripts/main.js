e1cd534e01264ba8a3b789769d12f004e91d25ce
Luiz Conde <luizconde.dev@outlook.com>
2025-02-21 14:41:38 -0300

    implement countdown timer for event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 AOS.init();
+
+const eventDate = new Date('2025-12-12 19:00:00');
+const eventTimeStamp = eventDate.getTime();
+
+const countHours = setInterval(function(){
+    const now = new Date();
+    const nowTimeStamp = now.getTime();
+
+    const distance = eventTimeStamp - nowTimeStamp;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('meter').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    if (distance < 0) {
+        clearInterval(countHours);
+        document.getElementById('meter').innerHTML = 'Evento Expirado';
+    }
+
+}, 1000);
